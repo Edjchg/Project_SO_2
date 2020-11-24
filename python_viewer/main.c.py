0ada4fd913fc294abcc573e9dc7cfcd008a1290b
Edjchg <edjchg@gmail.com>
2020-11-23 19:09:44 -0600

    Add cpu usage viewer

--- python_viewer/main.c.py
+++ python_viewer/main.c.py
@@ -4,6 +4,35 @@ import matplotlib.pyplot as plt
 def main():
     graph_total_time()
     graph_average_time()
+    graph_cpu_usage()
+
+def graph_cpu_usage():
+    fifo_cpu_usage = "../cliente/fifo_statistics/fifo_cpu_usage.txt"
+    # add other server path here...
+    # ·
+    # ·
+    # ·
+    list_cpu_usage = build_x_y_list(fifo_cpu_usage)
+    # build the x y list here...
+    # ·
+    # ·
+    # ·
+    if len(list_cpu_usage[0]) == 0 or len(list_cpu_usage[1]) == 0:
+        print("It can not plot inexisting points.")
+    else:
+        plt.figure()
+        plt.subplot(311)  # 311 means: 3 rows 1 column 1st element:
+        plt.title("Fifo server % CPU usage graph")
+        plt.xlabel("Elements")
+        plt.ylabel("% CPU usage")
+        plt.plot(list_cpu_usage[0], list_cpu_usage[1])
+        # add subplot for the other servers here
+        # ·
+        # ·
+        # ·
+        plt.subplots_adjust(top=0.92, bottom=0.08, left=0.145, right=0.95, hspace=0.65,
+                            wspace=0.35)
+        plt.show()
 
 
 def graph_average_time():
@@ -24,6 +53,8 @@ def graph_average_time():
         plt.figure()
         plt.subplot(311)  # 311 means: 3 rows 1 column 1st element:
         plt.title("Fifo server average time graph")
+        plt.xlabel("Average time (s)")
+        plt.ylabel("Elements")
         plt.plot(list_average_time[0], list_average_time[1])
         # add subplot for the other servers here
         # ·
@@ -52,6 +83,8 @@ def graph_total_time():
         plt.figure()
         plt.subplot(311)  # 311 means: 3 rows 1 column 1st element:
         plt.title("Fifo server total time graph")
+        plt.xlabel("Total time (s)")
+        plt.ylabel("Elements")
         plt.plot(list_total_time[0], list_total_time[1])
         # add subplot for the other servers here
         # ·
